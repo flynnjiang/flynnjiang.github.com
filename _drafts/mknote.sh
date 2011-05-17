ed08fdebd2f9a65deb6db79d1e9bd884c024c9d7
flynnjiang <flynnjiang@gmail.com>
2011-05-17 13:29:10 +0800

    edit note script

--- _drafts/mknote.sh
+++ _drafts/mknote.sh
@@ -1,4 +1,16 @@
 #/bin/bash
+VI=
+if [ -x /usr/bin/gvim ]; then
+	VI=/usr/bin/gvim
+elif [ -x /usr/bin/vim ]; then
+	VI=/usr/bin/vim
+elif [ -x /usr/bin/vi ]; then
+	VI=/usr/bin/vi
+else
+	return 127
+fi
 
-NOTE_TITLE=`date --rfc-3339=date`-$*.md
-gvim ${NOTE_TITLE// /-}
+FILE_NAME=`date --rfc-3339=date`-$*
+FILE_NAME=${FILE_NAME//[^a-zA-Z0-9\-]/-}.md
+
+$VI $FILE_NAME
