14e9b4622761ee3b679f2067efa2f0b0ecb1578d
Flynn Jiang <flynnjiang@gmail.com>
2012-07-05 21:17:12 +0800

    test blogger api

---
+++ js/plugins/blogger.js
@@ -0,0 +1,54 @@
+﻿(function($) {
+	$.fn.blogger = function(options) {
+		
+		var resp_callback = function(result) {
+			
+			target.html("努力加载中。。。");
+			
+			$(result.items).each(function() {
+				target.append('<a href="'+ this.url + '">' + this.title + '</a><span>' + this.published + '</span>');
+			});
+
+			
+			if ($result.prevPageToken) {
+				target.append(
+					'<a href="javascript:void(0)" onclick="$("#' + target_id + '").blogger({'
+					+ 'id: ' + options.id +','
+					+ 'key: ' + options.key + ','
+					+ 'page: ' + result.prevPageToken + 
+					+ '})">Prev</a>');
+			}
+			
+			if ($result.nextPageToken) {
+				target.append(
+					'<a href="javascript:void(0)" onclick="$("#' + target_id + '").blogger({'
+					+ 'id: ' + options.id +','
+					+ 'key: ' + options.key + ','
+					+ 'page: ' + result.nextPageToken + 
+					+ '})">Next</a>');
+			}
+			
+		};
+
+		var defaults = {
+			'id' : "5568048636599513853",
+			'key' : "AIzaSyD9TfhepQP6C50axzubBBPHMHoPFnDdcJw",
+			'page' : "",
+			'callback' : resp_callback
+		};
+
+		
+		var options = $.extend(defaults, options);
+		var target = this;
+		var target_id = target.attr("id");
+
+		$.getJSON(
+			'https://www.googleapis.com/blogger/v3/blogs/' + options.id + '/posts'
+			+ '?'
+			+ 'fields=items(published,title,updated,url),nextPageToken,prevPageToken'
+			+ (options.page ? ('&pageToken=' + option.page) : "")
+			+ '&key=' + options.key,
+			options.callback);
+
+	};
+})(jQuery);
